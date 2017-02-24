e033e349d87108356729ad8c7152c8af7d304bc1
Kyle Cohen <kyle_cohen@cacd.uscourts.gov>
2017-02-24 15:46:50 -0800

    Added EditNoteActivity activity; Started CalendarMain.java functionality for launching EditNoteActivity

--- app/src/main/java/kcohen/calendartry/CalendarMain.java
+++ app/src/main/java/kcohen/calendartry/CalendarMain.java
@@ -2,6 +2,11 @@ package kcohen.calendartry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.CalendarView;
+import android.content.Intent;
 
 public class CalendarMain extends AppCompatActivity {
 
@@ -10,4 +15,12 @@ public class CalendarMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_main);
     }
+
+    public void editNote(View view){
+
+        Intent intent = new Intent(this, EditNoteActivity.class);
+
+
+    }
+
 }
