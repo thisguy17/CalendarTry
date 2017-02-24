cd2e6f1f542def2c68fcbd4e173e6029055b6b01
Kyle Cohen <kyle_cohen@cacd.uscourts.gov>
2017-02-24 12:27:15 -0800

    first commit

---
+++ app/src/main/java/kcohen/calendartry/CalendarMain.java
@@ -0,0 +1,13 @@
+package kcohen.calendartry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_main);
+    }
+}
